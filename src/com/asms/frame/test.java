b39776cac4b38f12d20555d1324b3fbc9e485fcd
User-chenhua <1730020338@qq.com>
2019-08-14 17:02:01 +0800

    chenhua2

--- src/com/asms/frame/test.java
+++ src/com/asms/frame/test.java
@@ -3,10 +3,23 @@
  */
 package com.asms.frame;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * @author гугугу
  *
  */
 public class test {
-
+public static void main(String[] args) throws FileNotFoundException, IOException {
+	Screenshots ws = null;
+	try {
+		ws = new Screenshots();
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	ws.setVisible(true);
+}
 }
